5f4f02f87eccc946a747e246e67146c7fe262bdb
benbeezy <benengel94@gmail.com>
2015-06-18 19:48:28 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,203 @@
+<html lang="en"><!--@(window)--><!--@(document)--><head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="../../favicon.ico">
+<link rel="stylesheet" href="./styles/custom.css">
+<title>Benjamin Engel</title>
+
+<!-- Bootstrap core CSS -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+
+<![endif]-->
+<meta name="chromesniffer" id="chromesniffer_meta" content="{&quot;jQuery&quot;:&quot;1.11.2&quot;}"><script type="text/javascript" src="chrome-extension://homgcnaoacgigpkkljjjekpignblkeae/detector.js"></script></head>
+
+<body>
+
+<a href="https://github.com/benbeezy"><img style="position: absolute; top: 51; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+<span class="sr-only">Toggle navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="#">Benjamin Engel</a>
+</div>
+<div id="navbar" class="navbar-collapse collapse">
+<ul class="nav navbar-nav">
+<li class="active"><a href="#">Home</a></li>
+<li><a href="#">About</a></li>
+<li><a data-toggle="modal" href="#defaultModal">Contact</a></li>
+<li class="dropdown">
+<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+<ul class="dropdown-menu" role="menu">
+<li><a href="#">Action</a></li>
+<li><a href="#">Another action</a></li>
+<li><a href="#">Something else here</a></li>
+<li class="divider"></li>
+<li class="dropdown-header">Nav header</li>
+<li><a href="#">Separated link</a></li>
+<li><a href="#">One more separated link</a></li>
+</ul>
+</li>
+</ul>
+</div><!--/.navbar-collapse -->
+</div>
+</nav>
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+<div class="container">
+<h1>Hello, world!</h1>
+<p>
+My name is Benjamin Engel
+
+<div class="row">
+<div class="col-md-6">
+<ul>
+
+<li>
+20 years old
+</li>
+<li>
+Web developer for 3 years
+</li>
+<li>
+I know PHP, JavaScript, HTML5, SQL, SASS CSS, and LESS CSS
+</li>
+<li>
+I have been learning python, java, and C
+</li>
+
+</ul>
+</div>
+
+<div class="col-md-6">
+<ul>
+
+<li>
+3D modeling: Blender, Openscad, Maya, 123D design.
+</li>
+<li>
+I build 3D printers
+</li>
+<li>
+I was a google glass dev
+</li>
+<li>
+Google Tango dev
+</li>
+
+</ul>
+</div>
+</div>
+
+</p>
+</div>
+</div>
+
+<div class="container">
+<!-- Example row of columns -->
+<div class="row">
+<div class="col-md-4">
+<div class="row">
+<div class="col-md-12">
+<h2>Find me at</h2>
+</div>
+</div>
+<div class="row">
+<div class="col-md-6">
+    <a class="btn btn-default instructables" href="http://www.instructables.com/member/BenjaminE4/" role="button">Instructables <i class="fa fa-wrench"></i></a>
+</div>
+<div class="col-md-6">
+    <a class="btn btn-default github" href="http://github.com/benbeezy" role="button">github <i class="fa fa-github"></i></a>
+</div>
+</div>
+<div class="row">
+<div class="col-md-6">
+    <a class="btn btn-default google_plus" href="http://www.instructables.com/member/BenjaminE4/" role="button">google plus <i class="fa fa-google-plus"></i></a>
+</div>
+<div class="col-md-6">
+    <a class="btn btn-default youtube" href="http://www.instructables.com/member/BenjaminE4/" role="button">youtube <i class="fa fa-youtube"></i></a>
+</div>
+</div>
+
+</div>
+
+<div class="col-md-4">
+<h2>Dollo 3D printer</h2>
+<p>I created the Dollo 3D as a proofe of consept, to show people that yes, a 3D printer can indeed print another 3D printer. Then I took it a lever more and made all of the models parametric, and the structure modular. So this printer can print its own extention pieces, to make its self larger, and it has a swappable head so that you can eithor laser-but CNC mill or 3D print with it,</p>
+<p><a class="btn btn-default" href="https://github.com/benbeezy/Dollo" role="button">Print one <i class="fa fa-github-alt"></i></a></p>
+</div>
+<div class="col-md-4">
+<h2>Dollo.org</h2>
+<p>I love opensrource, and all of the good 3D model distrobution platforms were going closed source. So for a startup weekend project my dad and I came up with and made a git bases 3D model sharing platform that also connected to machines so they could get their idea put into production in a local setting instead of ordering things from china.</p>
+<p><a class="btn btn-default" href="#" role="button">Fork a project <i class="fa fa-code-fork"></i></a></p>
+</div>
+</div>
+<div class="row">
+<div class="col-md-4">
+<h2>FPV racing Quadcopter</h2>
+<p>I saw a video online about doing First Person View on a racing quadcopter and just knew that I had to make one of my own. I of course made it parametric with things like changing the weight of the frame, and what size of props you want. </p>
+<p><a class="btn btn-default" href="./openscad" role="button">Make your own <i class="fa fa-gamepad"></i></a></p>
+</div>
+<div class="col-md-4">
+<h2>parametric VR Headset</h2>
+<p>Google cardboard is a pretty cool platform to play around with, but the issue I found was that it does not work with everyones phones, and some people want to use different lenses and have different face shapes. I used Openscad to make a parametric 3D printed version of google cardboard, meaning that you can programatically generate a VR headset that fits your needs.</p>
+<p><a class="btn btn-default" href="./openscad" role="button">Make your own <i class="fa fa-terminal"></i></a></p>
+<div>
+</div>
+
+
+<div id="defaultModal" class="modal fade">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+<h4 id="modal-label">Example Without Pull Right Modal</h4>
+</div>
+<div class="modal-body">
+<div class="row">
+PHONE: (435) 862-4116
+</div>
+<div class="row">
+EMAIL: benengel94@gmail.com
+</div>
+<div class="row">
+TWITTER: @Ben_Beezy
+</div>
+<div class="row">
+HANGOUTS: benengel94 -- best way to contact me
+</div>
+</div>
+</div> <!--modal-content-->
+</div> <!--modal-dialog-->
+</div> <!--modal-->
+
+<footer>
+</footer>
+</div> <!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+
+<div id="window-resizer-tooltip" style="display: none;"><a href="#" title="Edit settings"></a><span class="tooltipTitle">Window size: </span><span class="tooltipWidth" id="winWidth">1636</span> x <span class="tooltipHeight" id="winHeight">1323</span><br><span class="tooltipTitle">Viewport size: </span><span class="tooltipWidth" id="vpWidth">1636</span> x <span class="tooltipHeight" id="vpHeight">835</span></div></body></html>
